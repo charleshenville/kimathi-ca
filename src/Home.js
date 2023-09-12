1d3d4a07fb1b8e08d7a860be3beb963f8d19b4c7
Charles Henville <henvillecharles@gmail.com>
2023-09-11 20:49:48 -0400

    Thumbhotfix

--- src/Home.js
+++ src/Home.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 
 function Home() {
 
-  function cf(string) {
-    console.log(string)
-    return string.substring(0, 1).toUpperCase() + string.substring(1);
-  }
+  // function cf(string) {
+  //   console.log(string)
+  //   return string.substring(0, 1).toUpperCase() + string.substring(1);
+  // }
 
   return (
     <div className="App">
@@ -21,9 +21,9 @@ function Home() {
 
                 {(item.thumb.endsWith('.mp4') || item.thumb.endsWith('.mov')) ?
                   <video className='media' id="video" autoPlay loop muted playsInline>
-                    <source src={cf(item.thumb)} type="video/mp4" />
+                    <source src={item.thumb} type="video/mp4" />
                   </video> :
-                  <img className='media' src={cf(item.thumb)} />}
+                  <img className='media' src={item.thumb} />}
                 <h2 style={{ position: 'absolute', top: '30%', zIndex: '1', pointerEvents: 'none' }}>{item.name}</h2>
                 <p style={{ position: 'absolute', top: '45%', zIndex: '1', pointerEvents: 'none' }}>{item.year}</p>
 
